64ec80226356f4bac5102eab9173bb0e4f48412c
DESKTOP-KT6GSMQ\Ilya <win7user10@yandex.ru>
2024-02-04 20:12:20 +0300

    add missing file

---
+++ src/Laraue.Apps.LearnLanguage.Services/Repositories/Contracts/UserViewSettings.cs
@@ -0,0 +1,7 @@
+﻿using Laraue.Apps.LearnLanguage.DataAccess.Enums;
+
+namespace Laraue.Apps.LearnLanguage.Services.Repositories.Contracts;
+
+public record UserViewSettings(
+    WordsTemplateMode WordsTemplateMode,
+    ShowWordsMode ShowWordsMode);
