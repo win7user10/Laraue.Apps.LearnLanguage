f4918343d34c3a8d2b4794999ca34c1239c04488
DESKTOP-KT6GSMQ\Ilya <win7user10@yandex.ru>
2025-08-28 11:21:35 +0300

    temp save

---
+++ src/Laraue.Apps.LearnLanguage.Services/Services/TelegramUserQueryService.cs
@@ -0,0 +1,26 @@
+﻿using Laraue.Apps.LearnLanguage.DataAccess;
+using Laraue.Telegram.NET.Authentication.Services;
+using Microsoft.EntityFrameworkCore;
+using User = Laraue.Apps.LearnLanguage.DataAccess.Entities.User;
+
+namespace Laraue.Apps.LearnLanguage.Services.Services;
+
+public class TelegramUserQueryService(DatabaseContext context)
+    : ITelegramUserQueryService<User, Guid>
+{
+    public Task<User?> FindAsync(long telegramId)
+    {
+        return context.Users
+            .Where(u => u.TelegramId == telegramId)
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<Guid> CreateAsync(User user)
+    {
+        context.Users.Add(user);
+        
+        await context.SaveChangesAsync();
+        
+        return user.Id;
+    }
+}
